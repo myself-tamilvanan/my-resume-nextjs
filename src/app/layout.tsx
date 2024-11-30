f2526d6fe370f6daccc3908b438151a7eca2cc10
Tamilvanan <tamilvanan@cannydigit.com>
2024-11-30 23:58:49 +0530

    Completed Resume

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import { Nunito } from "next/font/google";
+import "../styles/style.sass";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const primaryFont = Nunito({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--primary-font",
 });
 
 export const metadata: Metadata = {
@@ -25,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${primaryFont.variable} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ src/assets/index.ts
@@ -0,0 +1,141 @@
+import img_agile_github from "./images/agile/img-agile-github.png";
+import img_agile_bitbucket from "./images/agile/img-agile-bitbucket.png";
+import img_agile_gitlab from "./images/agile/img-agile-gitlab.png";
+import img_agile_jira from "./images/agile/img-agile-jira.png";
+import img_architecture_atomic from "./images/architecture/img-architecture-atomic.png";
+import img_architecture_microservices from "./images/architecture/img-architecture-microservices.png";
+import img_architecture_mono_repo from "./images/architecture/img-architecture-mono-repo.jpg";
+import img_architecture_mvc from "./images/architecture/img-architecture-mvc.jpg";
+import img_architecture_nx_tool from "./images/architecture/img-architecture-nx-tool.png";
+import img_architecture_turbo from "./images/architecture/img-architecture-turbo.png";
+import img_db_mysql from "./images/database/img-db-mysql.png";
+import img_db_postgreql from "./images/database/img-db-postgreql.png";
+import img_db_prisma_orm from "./images/database/img-db-prisma-orm.png";
+import img_framework_android from "./images/framework/img-framework-android.png";
+import img_framework_django from "./images/framework/img-framework-django.svg";
+import img_framework_expressjs from "./images/framework/img-framework-expressjs.webp";
+import img_framework_flutter from "./images/framework/img-framework-flutter.png";
+import img_framework_nestjs from "./images/framework/img-framework-nestjs.svg";
+import img_framework_nextjs from "./images/framework/img-framework-nextjs.svg";
+import img_framework_react_js from "./images/framework/img-framework-react-js.png";
+import img_framework_react_native from "./images/framework/img-framework-react-native.webp";
+import img_framework_vuejs from "./images/framework/img-framework-vuejs.png";
+import img_hosting_apache from "./images/hosting/img-hosting-apache.svg";
+import img_hosting_aws_ec2 from "./images/hosting/img-hosting-aws-ec2.png";
+import img_hosting_caddy from "./images/hosting/img-hosting-caddy.jpg";
+import img_hosting_docker from "./images/hosting/img-hosting-docker.webp";
+import img_hosting_firebase from "./images/hosting/img-hosting-firebase.png";
+import img_hosting_nginx from "./images/hosting/img-hosting-nginx.png";
+import img_hosting_playstore from "./images/hosting/img-hosting-playstore.jpg";
+import img_hosting_private_server from "./images/hosting/img-hosting-private-server.webp";
+import img_hosting_vercel from "./images/hosting/img-hosting-vercel.png";
+import img_ide_android_studio from "./images/ide/img-ide-android-studio.webp";
+import img_ide_intellij from "./images/ide/img-ide-intellij.png";
+import img_ide_pycharm from "./images/ide/img-ide-pycharm.png";
+import img_ide_visual_studio_code from "./images/ide/img-ide-visual-studio-code.png";
+import img_lang_dart from "./images/languages/img-lang-dart.png";
+import img_lang_java from "./images/languages/img-lang-java.png";
+import img_lang_javascript from "./images/languages/img-lang-javascript.webp";
+import img_lang_kotlin from "./images/languages/img-lang-kotlin.png";
+import img_lang_sass from "./images/languages/img-lang-sass.png";
+import img_lang_typescript from "./images/languages/img-lang-typescript.png";
+import img_tech_firebase from "./images/technical skills/img-tech-firebase.png";
+import img_tech_graph_ql from "./images/technical skills/img-tech-graph-ql.png";
+import img_tech_pwa from "./images/technical skills/img-tech-pwa.webp";
+import img_tech_react_query from "./images/technical skills/img-tech-react-query.png";
+import img_tech_redux from "./images/technical skills/img-tech-redux.svg";
+import img_tech_rest_api from "./images/technical skills/img-tech-rest-api.png";
+import img_tech_swagger from "./images/technical skills/img-tech-swagger.jpg";
+import img_logo_fanam from "./images/logo-apps/img_logo_fanam.png";
+import img_logo_wnb from "./images/logo-apps/img_logo_wnb.jpg";
+import img_logo_qmin from "./images/logo-apps/img_logo_qmin.png";
+import img_logo_aab from "./images/logo-apps/img_logo_aab.png";
+import img_logo_homefy from "./images/logo-apps/img_logo_homefy.png";
+import img_logo_catering from "./images/logo-apps/img_logo_catering.png";
+import img_logo_qmin_cafe from "./images/logo-apps/img_logo_qmin_cafe.png";
+import img_logo_satisfi from "./images/logo-apps/img_logo_satisfi.png";
+import img_logo_stuant from "./images/logo-apps/img_logo_stuant.jpg";
+import img_logo_religen from "./images/logo-apps/img_logo_religen.png";
+import img_logo_church from "./images/logo-apps/img_logo_church.png";
+import img_org_canny from "./images/organization/img_org_canny.jpg";
+import img_org_ramanujan_it_park from "./images/organization/img_org_ramanujan_it_park.svg";
+import img_org_rane from "./images/organization/img_org_rane.jpg";
+import img_org_seoyone from "./images/organization/img_org_seoyone.jpg";
+import img_profile from "./images/profile/img-profile.jpg";
+import img_email from "./images/profile/img_email.png";
+import img_github from "./images/profile/img_github.png";
+import img_linkedin from "./images/profile/img_linkedin.png";
+import img_phone from "./images/profile/img_phone.png";
+
+const assets = {
+  img_org_canny,
+  img_org_ramanujan_it_park,
+  img_org_rane,
+  img_org_seoyone,
+  img_logo_church,
+  img_logo_religen,
+  img_logo_stuant,
+  img_logo_satisfi,
+  img_logo_homefy,
+  img_logo_aab,
+  img_logo_qmin,
+  img_logo_qmin_cafe,
+  img_logo_wnb,
+  img_logo_catering,
+  img_agile_github,
+  img_agile_bitbucket,
+  img_agile_gitlab,
+  img_agile_jira,
+  img_architecture_atomic,
+  img_architecture_microservices,
+  img_architecture_mono_repo,
+  img_architecture_mvc,
+  img_architecture_nx_tool,
+  img_architecture_turbo,
+  img_db_mysql,
+  img_db_postgreql,
+  img_db_prisma_orm,
+  img_framework_android,
+  img_framework_django,
+  img_framework_expressjs,
+  img_framework_flutter,
+  img_framework_nestjs,
+  img_framework_nextjs,
+  img_framework_react_js,
+  img_framework_react_native,
+  img_framework_vuejs,
+  img_hosting_apache,
+  img_hosting_aws_ec2,
+  img_hosting_caddy,
+  img_hosting_docker,
+  img_hosting_firebase,
+  img_hosting_nginx,
+  img_hosting_playstore,
+  img_hosting_private_server,
+  img_hosting_vercel,
+  img_ide_android_studio,
+  img_ide_intellij,
+  img_ide_pycharm,
+  img_ide_visual_studio_code,
+  img_lang_dart,
+  img_lang_java,
+  img_lang_javascript,
+  img_lang_kotlin,
+  img_lang_sass,
+  img_lang_typescript,
+  img_tech_firebase,
+  img_tech_graph_ql,
+  img_tech_pwa,
+  img_tech_react_query,
+  img_tech_redux,
+  img_tech_rest_api,
+  img_tech_swagger,
+  img_logo_fanam,
+  img_profile,
+  img_email,
+  img_github,
+  img_linkedin,
+  img_phone,
+};
+
+export default assets;
